9d5e3df1c7de46f74b2f47a96a90b062992b2443
CCPENG123 <951134746@qq.com>
2020-04-07 21:41:59 +0800

    第三次提交代码，完善校验商家码数据的sql

--- src/main/java/cn/ccp/FunctionApplication.java
+++ src/main/java/cn/ccp/FunctionApplication.java
@@ -16,8 +16,10 @@ public class FunctionApplication {
         String xlsPath = "";//xls文件路径 eg:C:\Users\CCPENG\Desktop\印刷聚合二维码 (扫呗智慧零售2000个聚合码_20200225).xls
         String sheetName = "";//xls表名 eg:score
         String txtPath = "";//txt文件路径 eg:C:\Users\CCPENG\Desktop\商家码sql.txt
+        String verfitySqlPath = "";//校验商家码Sql
         //生成txt对象
         TxTData txt = new TxTData(txtPath);
+        TxTData txt2 = new TxTData(verfitySqlPath);
         try {
             HSSFSheet sheet = ExcelUtils.readXls(xlsPath, sheetName);
             //处理数据
@@ -27,6 +29,7 @@ public class FunctionApplication {
             String sql = "";
             //打开文件流
             txt.open();
+            txt2.open();
             for (int i = 1; i <= rows; i++) {
                 HSSFRow row = sheet.getRow(i);// 行数
                 //读取数据前设置单元格类型
@@ -36,9 +39,13 @@ public class FunctionApplication {
                 //得到sql
                 sql = ExcelUtils.generateSQL(roll1, roll2);
                 //将sql写入txt文件中
-                txt.wirteTxt(sql+"\r\n");
+                txt.wirteTxt(sql + "\r\n");
             }
-            System.out.println("一共处理"+rows+"条数据！");
+            //写入将校验的sql
+            String verfitySql = ExcelUtils.unquieSql.toString();
+            verfitySql = verfitySqlPath.substring(0,verfitySqlPath.length()-1)+");";
+            txt2.wirteTxt(verfitySql);
+            System.out.println("一共处理" + rows + "条数据！");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
